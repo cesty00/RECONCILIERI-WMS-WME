3c51ef2b96849655f471ad35c849c61eef3d5a0a
cesty <vaduva.cezar@yahoo.com>
2026-06-03 09:50:35 +0300

    Export document normalization utilities

---
+++ app/normalization/__init__.py
@@ -0,0 +1,17 @@
+from app.normalization.documents import (
+    NormalizedDocument,
+    canonical_document,
+    candidate_document_keys,
+    compact_document,
+    document_key_from_type_and_number,
+    normalize_document_reference,
+)
+
+__all__ = [
+    "NormalizedDocument",
+    "canonical_document",
+    "candidate_document_keys",
+    "compact_document",
+    "document_key_from_type_and_number",
+    "normalize_document_reference",
+]
